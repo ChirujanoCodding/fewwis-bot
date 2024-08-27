85c099a964b846894349790cec91b069929a3fe6
romandev <trdfabris@gmail.com>
2024-08-27 00:33:52 -0300

    refactor: ♻️ separated functionalities in compile command

--- src/commands/utilities/compile.rs
+++ src/commands/utilities/compile.rs
@@ -1,11 +1,9 @@
 use crate::{helper::Colors, Context, Error};
-use lazy_regex::regex_captures;
 use pistones::client::ClientBuilder;
 
 use ::serenity::builder::CreateEmbed;
 use poise::{CodeBlock, CreateReply};
 use serenity::all::CreateEmbedFooter;
-use tracing::error;
 
 #[poise::command(prefix_command, track_edits, category = "Utilities")]
 pub async fn compile(
@@ -14,14 +12,25 @@ pub async fn compile(
     #[description = "the code to run"]
     code: String,
 ) -> Result<(), Error> {
-    let Some((_, lang, code)) =
-        regex_captures!("```(?<lang>[^\n]*)\n(?<code>.+?)\n```"sm, code.as_str())
-    else {
-        return Err("Cannot parse the code".into());
+    let reply = CreateReply::default();
+
+    let Ok((lang, code)) = parse_code(&code) else {
+        return Err("Error parsing the code.".into());
     };
 
-    let duration = std::time::Instant::now();
+    let data = compile_code(lang, code).await.unwrap();
 
+    let embed = create_result_embed(data);
+
+    ctx.send(reply.embed(embed)).await.unwrap();
+
+    Ok(())
+}
+
+async fn compile_code(
+    lang: &str,
+    code: &str,
+) -> Result<pistones::lang::Response, pistones::error::Error> {
     let client = ClientBuilder::new()
         .set_lang(lang)
         .set_main_file(code)
@@ -29,42 +38,44 @@ pub async fn compile(
         .build()
         .unwrap();
 
-    let Ok(response) = client.execute().await else {
-        error!("IMPORTANT ERROR HERE");
-        return Err("error executing the code...".into());
-    };
+    client.execute().await
+}
 
+fn parse_code(code: &str) -> Result<(&str, &str), Error> {
+    lazy_regex::regex_captures!("```(?<lang>[^\n]*)\n(?<code>.+?)\n```"sm, code)
+        .map_or(Err("Error parsing the code".into()), |(_, lang, code)| {
+            Ok((lang, code))
+        })
+}
+
+fn create_result_embed(response: pistones::lang::Response) -> CreateEmbed {
     let data = response.data();
+    let success = data.code() == 0;
+    let (emoji, color) = if success {
+        ("👩‍🍳", Colors::Fewwis)
+    } else {
+        ("💀", Colors::Red)
+    };
 
     let embed = CreateEmbed::new()
         .title(format!(
             "{} Your code is cooked! {} ({})",
-            if data.code() == 0 {
-                "👩‍🍳"
-            } else {
-                "💀"
-            },
+            emoji,
             response.language(),
             response.version()
         ))
-        .color(if data.code() == 0 {
-            Colors::Fewwis
-        } else {
-            Colors::Red
-        })
-        .description(
-            CodeBlock {
-                code: data.output().to_string(),
-                ..Default::default()
-            }
-            .to_string(),
-        )
-        .footer(CreateEmbedFooter::new(format!(
-            "elapsed: {}ms",
-            duration.elapsed().as_millis(),
-        )));
+        .color(color);
 
-    ctx.send(CreateReply::default().embed(embed)).await.unwrap();
+    let description = CodeBlock {
+        code: data.output().to_owned(),
+        ..Default::default()
+    };
 
-    Ok(())
+    if success {
+        embed
+            .description(description.to_string())
+            .footer(CreateEmbedFooter::new("compiled successfully"))
+    } else {
+        embed.description(format!("Error: {}", response.data().output()))
+    }
 }
